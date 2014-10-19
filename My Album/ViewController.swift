b67ab026f7f493774d7ed13f61e33c649eaf670e
Ranjith Nagella <nagella.ranjith@gmail.com>
2014-10-19 19:30:35 -0400

    - Added pan gesture. - Added rotation gesture.

--- My Album/ViewController.swift
+++ My Album/ViewController.swift
@@ -11,9 +11,22 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    var lastRotation = CGFloat()
+    let rotateRec = UIRotationGestureRecognizer()
+    let pinchRec = UIPinchGestureRecognizer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        rotateRec.addTarget(self, action: "rotatedView:")
+        pinchRec.addTarget(self, action: "pinchedView:")
+        
+        imageView.addGestureRecognizer(rotateRec)
+        imageView.addGestureRecognizer(pinchRec)
+        
+        imageView.userInteractionEnabled = true
+        imageView.multipleTouchEnabled = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,5 +51,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismissViewControllerAnimated(true, completion: nil)
     }
 
+    @IBAction func addLabelButtonPressed(sender: AnyObject) {
+        println("add label")
+    }
+    
+    func rotatedView(sender: UIRotationGestureRecognizer) {
+        println("rotate view")
+        var lastRotation = CGFloat()
+//        self.view.bringSubviewToFront(rotateView)
+        if(sender.state == UIGestureRecognizerState.Ended){
+            lastRotation = 0.0;
+        }
+        var rotation = 0.0 - (lastRotation - sender.rotation)
+        var point = rotateRec.locationInView(imageView)
+        var currentTrans = sender.view?.transform
+        var newTrans = CGAffineTransformRotate(currentTrans!, rotation)
+        sender.view?.transform = newTrans
+        lastRotation = sender.rotation
+    }
+    
+    func pinchedView(sender: UIPinchGestureRecognizer) {
+        
+        sender.view?.transform = CGAffineTransformScale(sender.view!.transform, sender.scale, sender.scale)
+        sender.scale = 1.0
+    }
 }
 
