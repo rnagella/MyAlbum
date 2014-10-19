ce33e8a7e4a6894adae71fe78035084167963d6c
Ranjith Nagella <nagella.ranjith@gmail.com>
2014-10-19 17:01:25 -0400

    - Capture images from save album.

--- My Album/ViewController.swift
+++ My Album/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func imagesButtonPresssed(sender: AnyObject) {
+        
+        var imageViewController = UIImagePickerController()
+        imageViewController.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum
+        imageViewController.delegate = self
+        presentViewController(imageViewController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        println("did finish? yes")
+        var image = info[UIImagePickerControllerOriginalImage] as UIImage
+        imageView.image = image
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
